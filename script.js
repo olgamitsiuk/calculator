3096e6d8dc855c035372b7c77e1b25df14d38e20
Mitsiuk Olha <mitsyuk.olga@gmail.com>
2024-10-13 23:09:24 +0100

    add functionality

--- script.js
+++ script.js
@@ -1,32 +1,98 @@
-function calc(op) {
+const result = document.getElementById('result'),
+      expression = document.getElementById('expression'),
+      num = document.querySelectorAll('.number:not(.equals)'),
+      operator = document.querySelectorAll('.operator'),
+      equals = document.querySelector('.equals'),
+      clear = document.getElementById('clear'),
+      ce = document.getElementById('ce');
+let ex = ''; // the expression string to be eval'd
+result.innerHTML = '0';
 
-  var result;
-  var num1 = Number(document.getElementById("num1").value);
-  var num2 = Number(document.getElementById("num2").value);
-  console.log(typeof num1)
-
-  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    result = "Invalid input: both arguments must be numbers.";
+function clickN() { // when we click on a number
+  if(!ex || typeof(ex) === 'number' || ex === '0') {
+    expression.innerHTML = this.id;
+    ex = this.id;
+  } else {
+    expression.innerHTML += this.id;
+    ex += this.id;
   }
+  result.innerHTML = ex.split(/\/|\*|\+|-|=/).pop();
+  checkLength(result.innerHTML);
+};
 
-  switch (op) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-        if (num2 === 0) {
-            result = "Error: Division by zero is not allowed.";
-        } else {
-      result = num1 / num2;
-        }
-      break;
+function clickO() { // when we click on an operation
+  if(!ex) {
+    return;
   }
+  ex = ex.toString().replace(/=/, '');
+  if (ex.match(/\/|\*|\+|-|=/)) {
+    ex = eval(ex).toString();
+  } 
+  expression.innerHTML = expression.innerHTML.replace(/=/, '') + this.id;
+  ex += this.id;
+  result.innerHTML = this.id;
+};
 
-  document.getElementById("result").innerHTML = result;
+
+
+Array.from(num).forEach(function(element) { // assign appropriate function to all numbers and operations
+      element.addEventListener('click', clickN);
+    });
+
+Array.from(operator).forEach(function(element) {
+      element.addEventListener('click', clickO);
+    });
+
+// clear all on click
+clear.addEventListener('click', () => {
+  result.innerHTML = '';
+  expression.innerHTML = '';
+  ex = '';
+})
+
+// clear last entry on click
+ce.addEventListener('click', () => {
+  if (!expression.innerHTML.match(/=$/)) {
+    
+    expression.innerHTML = doCE(expression.innerHTML);
+    ex = doCE(ex); 
+    result.innerHTML = 0;
+    
+    function doCE(arg) {
+      arg = arg.split(/([\/\*\+\-\=])/g);
+      arg.splice(-1, 1);
+      return arg.join('');
+    }
+  }
+})
+
+// calculate the whole thing
+equals.addEventListener('click', ()=> {
+  if (!ex) {
+    result.innerHTML = '0';
+  } else {
+    ex = eval(ex);
+    expression.innerHTML += '=';
+    result.innerHTML = trim12(ex);
+  }
+})
+
+function checkLength(arg) { // if we enter a number that's too long 
+  if (arg.toString().length > 14) {
+    expression.innerHTML = 'number too long'.toUpperCase();
+    result.innerHTML = '0';
+    ex = '0';
+  } 
+}
+
+function trim12(arg) { // if we calculate a number that's too long
+  if (arg.toString().length > 14) {
+    ex = parseFloat(arg.toPrecision(12));
+    if (ex.toString().length > 14) { 
+      ex = ex.toExponential(9);
+    };
+    return ex;
+  } else {
+    return arg;
+  }
 }
